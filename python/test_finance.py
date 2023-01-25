15d64f779c60a71e81a33ac34904eb7f6684347b
Bisby91 <cristianbisby@gmail.com>
2023-01-25 21:57:10 +0000

    A few improvement added: Timing decorator method to measure the time of execution. Doc strings added to each method. Typed arguments added. SelectStock divided in two methods. select_stock_comprehension() and check_profit_comprehension() added. 3 tests more addedtest_finance_works_720_savings() extra test for selectStock() and test_finance_works_comprehension_250_savings(), test_finance_works_comprehension_720_savings() to test the comprehension methods. Requirements file created. README edited.

--- python/test_finance.py
+++ python/test_finance.py
@@ -1,14 +1,35 @@
 import unittest
-from finance import selectStock
+from finance import select_stock_comprehension, selectStock
 
 
 class TestFinance(unittest.TestCase):
     def setUp(self) -> None:
         return super().setUp()
     
-    def test_finance_works(self):
-        saving = 250
+    def test_finance_works_250_savings(self) -> None:
+        savings = 250
         current_value = [175,133,109,210,97]
         future_value = [200,125,128,228,133]
-        result = selectStock(saving, current_value, future_value)
+        result = selectStock(savings, current_value, future_value)
         self.assertEqual(result, 55)
+
+    def test_finance_works_720_savings(self) -> None:
+        savings = 450
+        current_value = [220,135,209,110,84]
+        future_value = [95,299,201,99,147]
+        result = selectStock(savings, current_value, future_value)
+        self.assertEqual(result, 227)
+
+    def test_finance_works_comprehension_250_savings(self) -> None:
+        savings = 250
+        current_value = [175,133,109,210,97]
+        future_value = [200,125,128,228,133]
+        result = select_stock_comprehension(savings, current_value, future_value)
+        self.assertEqual(result, 55)
+        
+    def test_finance_works_comprehension_720_savings(self) -> None:
+        savings = 450
+        current_value = [220,135,209,110,84]
+        future_value = [95,299,201,99,147]
+        result = select_stock_comprehension(savings, current_value, future_value)
+        self.assertEqual(result, 227)
